f11fd3e45ef5083bf1a1ebdd25d936b9a4e53c29
Viktor <malyshkin.viktor@icloud.com>
2021-04-05 19:00:22 +0300

    task #3 -  Practicing Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
   styles: [`
-    h3 {
-      color: darkblue;
+    .white {
+      color: white;
     }
   `],
 })
 export class AppComponent {
+  isParagraph: boolean = false;
+  logs: any[] = [];
+
+  onParagraph(event): void {
+    this.isParagraph = !this.isParagraph;
+    console.log(new Date(event.timeStamp));
+    this.logs.push(new Date(event.timeStamp));
+  }
+
+  backgroundColor(index): string {
+    return this.isCheckIndex(index) && 'blue';
+  }
+
+  isCheckIndex(index): boolean {
+    return index >= 4;
+  }
 }
